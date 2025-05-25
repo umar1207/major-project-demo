09fc03c023cbb1279edc4f6d2f206b1000f4dda8
umar <mdumarkhan.1207@gmail.com>
2025-05-25 12:33:08 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
                 <p>
                     This is a simple React app that uses Vite for development and build. It is a basic template to get you started with React and Vite.
                 </p>
+                <h2>
+                    Home Page
+                </h2
             </div>
         </>
     )
